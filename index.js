325be49d104a302ae901de854c3a378f74108d75
daeix <110390090+daeix@users.noreply.github.com>
2023-08-23 16:54:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+document.getElementById("submit-btn").addEventListener("click", pickColor);
+
+const schemeMode = document.getElementById("color-scheme-type");
+const colorPalette = document.getElementById("color-palette");
+const seedColor = document.getElementById("seed-color");
+
+function pickColor() {
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${seedColor.value.slice(1)}&mode=${
+      schemeMode.value
+    }&count=6
+    `
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const colorBar = data.colors
+        .map((colors) => {
+          return `
+                <div style="background-color:${colors.hex.value}" class="color">
+                  <div class="color-code">
+                    <p id="color-value">${colors.hex.value}</p>
+                  </div>
+                </div>
+        `;
+        })
+        .join("");
+      colorPalette.innerHTML = colorBar;
+    });
+}
